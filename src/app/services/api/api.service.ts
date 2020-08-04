673254316bec98cf0bd57ce9542e27d03ecd58d8
Rafee <rafeejenkins@gmail.com>
2020-08-05 00:59:37 +0100

    initial attempt

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpRequest, HttpEvent, HttpEventType } from '@angular/common/http';
 import { Product } from 'src/app/models/product';
 import { environment } from 'src/environments/environment';
+
+import { pipe, BehaviorSubject } from 'rxjs'
+import { map, last, tap } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +14,34 @@ export class ApiService {
   constructor(private http: HttpClient) { }
 
   getItemFromID(id: number) {
-    return this.http.get<Product>(`${environment.apiURL}TaoBao_Parser?id=${id}`)
+    const loaded = new BehaviorSubject(false);
+    
+    const request = new HttpRequest('GET', 
+    `${environment.apiURL}TaoBao_Parser?id=${id}`, 
+    { reportProgress: true });
+
+    return this.http.request(request).pipe(
+      map(event => this.getEventMessage(event)),
+      tap(message => this.showProgress(message)),
+      last()
+    )
+    //return this.http.get<Product>(`${environment.apiURL}TaoBao_Parser?id=${id}`)
+  }
+
+  showProgress(message) {
+    console.log(message)
+  }
+
+  private getEventMessage(event: HttpEvent<any>) {
+    switch (event.type) {
+      case HttpEventType.Sent:
+        return `Fetching has begun`;
+      case HttpEventType.DownloadProgress:
+        return `downloaded "${event.loaded}" bytes`;
+      case HttpEventType.Response:
+        return event.body
+      default:
+        return `surprising download event: ${event.type}.`;
+    }
   }
 }
