5f8221cf286813bab97415df8e79b4abf1935a73
Rafee <rafeejenkins@gmail.com>
2020-10-01 22:12:49 +0100

    Trying out nativ ng tags

--- src/app/features/haul-display/haul-display.component.ts
+++ src/app/features/haul-display/haul-display.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { ApiService } from 'src/app/services/api/api.service';
+import { Title, Meta } from '@angular/platform-browser';
 import { Profile } from 'src/app/models/user';
 import { Haul } from 'src/app/models/haul';
-import { MetafrenzyService } from 'ngx-metafrenzy';
+//import { MetafrenzyService } from 'ngx-metafrenzy';
 
 @Component({
   selector: 'app-haul-display',
@@ -14,7 +15,10 @@ export class HaulDisplayComponent implements OnInit {
 
   constructor(private activatedRoute: ActivatedRoute, 
               private api: ApiService,
-              private readonly meta: MetafrenzyService) {
+              private title: Title,
+              private meta: Meta
+              //private readonly meta: MetafrenzyService
+              ) {
                }
 
   public haul: Haul;
@@ -28,10 +32,13 @@ export class HaulDisplayComponent implements OnInit {
           console.debug(res.data());
           this.haul = res.data() as Haul
           this.getProfileData(res.data()["owner"])
-          this.meta.setMetaTag('og:title', this.haul.title)
-          this.meta.setMetaTag('og:url', `https://www.reviewmyrep.fashion/haul/${haulID}`)
-          this.meta.setMetaTag('og:image', `https://media.npr.org/assets/img/2017/09/12/macaca_nigra_self-portrait-3e0070aa19a7fe36e802253048411a38f14a79f8-s800-c85.jpg`)
-          this.meta.setMetaTag('og:type', 'article')
+          this.title.setTitle(this.haul.title)
+          this.meta.addTags([
+            {property: 'og:title', content: this.haul.title},
+            {property: 'og:url', content: `https://www.reviewmyrep.fashion/haul/${haulID}`},
+            {property: 'og:image', content: `https://media.npr.org/assets/img/2017/09/12/macaca_nigra_self-portrait-3e0070aa19a7fe36e802253048411a38f14a79f8-s800-c85.jpg`},
+            {property: 'og:type', content: 'article'},
+          ])
         })
       }
     })
@@ -40,7 +47,7 @@ export class HaulDisplayComponent implements OnInit {
   getProfileData(id) {
     this.api.getProfileFromID(id).subscribe(res => {
       this.creatorProfile = res.data() as Profile
-      this.meta.setMetaTag('og:description', `Click to checkout the haul from ${this.creatorProfile.displayName}`)
+      this.meta.addTag({property: 'og:description', content: `Click to checkout the haul from ${this.creatorProfile.displayName}`})
     })
   }
 
